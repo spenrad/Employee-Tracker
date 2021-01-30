8c4868348d6060ff34636093b5f3b950aeb34be0
Spencer Christy <spenrad@gmail.com>
2021-01-29 18:08:10 -0800

    added console.table and switch statements, index ready for case functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,5 +18,54 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-//   insert func to init here;
+    runPrompts();
 });
+
+function runPrompts() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Emloyees by Department",
+            "View All Employees by Role",
+            "Add Employee",
+            "Add Role",
+            "Add Department",
+            "Update Employee Roles"
+        ]
+    })
+    .then(function(answer) {
+        switch(answer.action) {
+        
+        case "View All Employees":
+            viewAll();
+            break;
+       
+        case "View All Employees by Department":
+            viewAllDept();
+            break;
+        
+        case "View All Employees by Role":
+            viewAllRole();
+            break;
+
+        case "Add Employee":
+            addEmploy();
+            break;
+
+        case "Add Role":
+            addRole();
+            break;
+
+        case "Add Department":
+            addDept();
+            break;
+
+        case "Update Employee Roles":
+            updateRoles();
+            break;
+        }
+    })
+}
