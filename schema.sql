b51ab25f69ec862a18650efaff77d281599d52de
Spencer Christy <spenrad@gmail.com>
2021-01-29 17:06:24 -0800

    updated schema to include foreign keys, updated seeds to agree with schema

--- schema.sql
+++ schema.sql
@@ -3,12 +3,10 @@ CREATE database employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee (
+
+CREATE TABLE department (
   ID INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT(10) NOT NULL,
-  manager_id INT (10) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (ID)
 );
 
@@ -17,11 +15,17 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT(10) NOT NULL,
-  PRIMARY KEY (ID)
+  PRIMARY KEY (ID),
+  FOREIGN KEY (department_id) REFERENCES department(ID)
 );
-  
-CREATE TABLE department (
+
+CREATE TABLE employee (
   ID INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (ID)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10) NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (role_id) REFERENCES role(ID),
+  FOREIGN KEY (manager_id) REFERENCES role(ID)
 );
--- seed.sql
+++ seed.sql
@@ -1,8 +1,8 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("James", "Birch",  22,  2), ("Sarah", "Shivers",  22,  2), ("Dolph", "Freely",  44,  4), ("Alberto", "Tunder",  44,  4), ("Jessica", "Stone",  8, null), ("Martina", "Lazlo",  8, null), ("Smokey", "Plessiasaur",  99,  4);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Front-end Engineer", 60000.00, 2), ("Back-end Engineer", 68000.00, 2), ("Manager", 110000, 1), ("Intern", 32000, 00);
-
 INSERT INTO department (name)
 VALUES ("Engineering"), ("Administration"), ("Student-Intern"); 
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Front-end Engineer", 60000.00, 1), ("Back-end Engineer", 68000.00, 1), ("Manager", 110000, 2), ("Intern", 32000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("James", "Birch",  1,  1), ("Sarah", "Shivers",  1,  1), ("Dolph", "Freely",  2,  1), ("Alberto", "Tunder",  2,  1), ("Jessica", "Stone",  3, null), ("Martina", "Lazlo",  3, null), ("Smokey", "Plessiasaur",  4,  4);
